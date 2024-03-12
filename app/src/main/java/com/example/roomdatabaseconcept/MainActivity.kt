a32faf7dee61854fd29fb943491f808eb89d359d
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-03-12 11:25:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/roomdatabaseconcept/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.roomdatabaseconcept
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.roomdatabaseconcept.ui.theme.RoomDatabaseConceptTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            RoomDatabaseConceptTheme {
+
+
+            }
+        }
+    }
+}
+
